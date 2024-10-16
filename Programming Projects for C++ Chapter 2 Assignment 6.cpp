0426281feafe770284dc5decc0879d89bfbb69a4
Ayden <133791707+The1Cook@users.noreply.github.com>
2024-10-16 14:35:51 -0500

    Update Programming Projects for C++ Chapter 2 Assignment 6.cpp

--- Programming Projects for C++ Chapter 2 Assignment 6.cpp
+++ Programming Projects for C++ Chapter 2 Assignment 6.cpp
@@ -1,19 +1,19 @@
 /*An employee is paid at a rate of $16.78 per hour for the first 40 hours
 worked in a week. Any hours over that are paid at the overtime rate of one and - one - half times that.
-From the worker’s gross pay, 6 % is withheld for
+From the workerâ€™s gross pay, 6 % is withheld for
 Social Security tax, 14 % is withheld for federal income tax, 5 % is withheld
 for state income tax, and $10 per week is withheld for union dues.If the
 worker has three or more dependents, then an additional $35 is withheld
 to cover the extra cost of health insurance beyond what the employer pays.
 
 Write a program that will read in the number of hours worked in a week
-and the number of dependents as input and will then output the worker’s
+and the number of dependents as input and will then output the workerâ€™s
 gross pay, each withholding amount, and the net take - home pay for the
 week.For a harder version, write your program so that it allows the calculation to be repeated as often as the user wishes.If this is a class exercise, ask
 your instructor whether you should do this harder version*/
 #include <iostream>
 #include<string>
-
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -25,23 +25,27 @@ int main()
         cin >> hours;
         int dependents = 0;
         cin >> dependents;
-        float grossPay = 16.78 * hours;
-        if (hours > 40)
-            float grossPay = (16.78 * hours) + ((hours - 40) * (16.78 * 1.5));
-        cout << "Gross Pay: " << grossPay << endl;
+        float grossPay = 0;
+        if (hours > 40) {
+            grossPay = ((16.78 * 40) + ((hours - 40) * (16.78 * 1.5)));
+        }
+        else {
+            grossPay = 16.78 * hours;
+        }
+        std::cout << "Gross Pay: $" << fixed << setprecision(2) << grossPay << endl;
         float AfterSocial = (grossPay * 0.06);
-        cout << "After Social Security tax: " << AfterSocial << endl;
+        std::cout << "After Social Security tax: $" << fixed << setprecision(2) << AfterSocial << endl;
         float AfterFed = (grossPay * 0.14);
-        cout << "After Federal Income Tax: " << AfterFed << endl;
+        std::cout << "After Federal Income Tax: $" << fixed << setprecision(2) << AfterFed << endl;
         float AfterState = (grossPay * 0.05);
-        cout << "After State Income Tax: " << AfterState << endl;
-        
+        std::cout << "After State Income Tax: $" << fixed << setprecision(2) << AfterState << endl;
+
         float AfterEverything = grossPay - AfterSocial - AfterFed - AfterState - 10;
         if (dependents >= 3)
             AfterEverything -= 35;
-        cout << "After all that boring stuff you get to take home $" << AfterEverything << endl;
+        std::cout << "After all that boring stuff you get to take home $" << fixed << setprecision(2) << AfterEverything << endl;
 
-        cout << "Again? 0 for yes, 1 for no. " << endl;
+        std::cout << "Again? 0 for yes, 1 for no. " << endl;
         cin >> k;
     }
 }
