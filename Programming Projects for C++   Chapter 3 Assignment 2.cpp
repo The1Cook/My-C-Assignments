d34bc862ee21a3419355b091da424b1d0441bf95
Ayden <133791707+The1Cook@users.noreply.github.com>
2024-10-16 13:36:36 -0500

    Update Programming Projects for C++   Chapter 3 Assignment 2.cpp

--- Programming Projects for C++   Chapter 3 Assignment 2.cpp
+++ Programming Projects for C++   Chapter 3 Assignment 2.cpp
@@ -13,13 +13,13 @@ int main() {
     if (discriminant > 0) {
         x1 = (-b + sqrt(discriminant)) / (2 * a);
         x2 = (-b - sqrt(discriminant)) / (2 * a);
-        cout << "Roots are real and different." << endl;
+        cout << "Roots are real and different" << endl;
         cout << "x1 = " << x1 << endl;
         cout << "x2 = " << x2 << endl;
     }
 
     else if (discriminant == 0) {
-        cout << "Roots are real and same." << endl;
+        cout << "Roots are real and same" << endl;
         x1 = -b / (2 * a);
         cout << "x1 = x2 =" << x1 << endl;
     }
