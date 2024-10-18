72a674b1db7c843bde16580a07698cfd520caffd
Ayden <133791707+The1Cook@users.noreply.github.com>
2024-10-18 13:42:27 -0500

    Update Programming Projects for C++ Chapter 2 Assignment 13.cpp

--- Programming Projects for C++ Chapter 2 Assignment 13.cpp
+++ Programming Projects for C++ Chapter 2 Assignment 13.cpp
@@ -1,21 +1,21 @@
 // Programming Projects for C++ Chapter 2 Assignment 13.cpp : This file contains the 'main' function. Program execution begins and ends there.
 /*
-The Harris–Benedict equation estimates the number of calories your body
+The Harrisâ€“Benedict equation estimates the number of calories your body
 needs to maintain your weight if you do no exercise. This is called your
 basal metabolic rate, or BMR.
 The formula for the calories needed for a woman to maintain her weight is
-BMR = 655 + (4.3 × weight in pounds) + (4.7 × height in inches) –
-(4.7 × age in years)
+BMR = 655 + (4.3 Ã— weight in pounds) + (4.7 Ã— height in inches) â€“
+(4.7 Ã— age in years)
 The formula for the calories needed for a man to maintain his weight is
-BMR = 66 + (6.3 × weight in pounds) + (12.9 × height in inches) –
-(6.8 × age in years)
+BMR = 66 + (6.3 Ã— weight in pounds) + (12.9 Ã— height in inches) â€“
+(6.8 Ã— age in years)
 
 
 A typical chocolate bar will contain around 230 calories. Write a program
 that allows the user to input his or her weight in pounds, height in
 inches, age in years, and the character M for male and F for female. The
 program should then output the number of chocolate bars that should
-be consumed to maintain one’s weigt
+be consumed to maintain oneâ€™s weigt
 */
 
 #include <iostream>
@@ -38,7 +38,7 @@ int main()
     cout << "What is your height in inches? " << endl;
     cin >> height;
     if (man == 'F') 
-        BMR = 655 + (4.3 * weight) + (4.7 * height) - (6.8 * age);
+        BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
     
     if (man == 'M')
         BMR = 66 + (6.3 * weight) + (12.9 * height) - (6.8 * age);
