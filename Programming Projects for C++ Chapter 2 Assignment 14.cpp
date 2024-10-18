fef165ee23cc314e52a62c1d52ebe8b70e4408b3
Ayden <133791707+The1Cook@users.noreply.github.com>
2024-10-18 13:41:46 -0500

    Update Programming Projects for C++ Chapter 2 Assignment 14.cpp

--- Programming Projects for C++ Chapter 2 Assignment 14.cpp
+++ Programming Projects for C++ Chapter 2 Assignment 14.cpp
@@ -9,6 +9,7 @@ points earned divided by the total points possible) and output it as a percentag
 #include <iostream>
 #include <array>
 #include <vector>
+#include <iomanip>
 using namespace std;
 
 
@@ -31,17 +32,6 @@ int main()
             overallTotal += total;
     }
    
-    cout << "Your total is " << overallScore << " out of " << overallTotal << ", or " << (overallScore / overallTotal) * 100 << "%.";
+    cout << "Your total is " << overallScore << " out of " << overallTotal << ", or " << fixed << setprecision(2) << (overallScore / overallTotal) * 100 << "%.";
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
