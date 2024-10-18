8327a27d2906e7636fc74914f6de5d6a8f2401ae
Ayden <133791707+The1Cook@users.noreply.github.com>
2024-10-18 13:33:58 -0500

    Update Programming Projects for C++ Chapter 2 Assignment 7.cpp

--- Programming Projects for C++ Chapter 2 Assignment 7.cpp
+++ Programming Projects for C++ Chapter 2 Assignment 7.cpp
@@ -2,7 +2,7 @@
 //
 
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -16,19 +16,9 @@ int main()
 	double inflationRate = 0.056;
 	cout << "What is the inflation rate percent in decimal form? ";
 	cin >> inflationRate;
-	for (int k = 0; k < years; k += 2) {
+	inflationRate /= 100;
+	for (int k = 0; k < years; k += 1) {
 		itemCost += itemCost * inflationRate;
 	}
-	cout << "Item cost after " << years << " years is $" << itemCost << endl;
+	cout << "Item cost after " << years << " years is $" << fixed << setprecision(2) << itemCost << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
