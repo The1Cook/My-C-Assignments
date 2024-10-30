14b06893bcb224b473835be38dadce001b42224b
Ayden <133791707+The1Cook@users.noreply.github.com>
2024-10-30 13:44:15 -0500

    Update Programming Projects for C++ Chapter 3 Assignment 1.cpp

--- Programming Projects for C++ Chapter 3 Assignment 1.cpp
+++ Programming Projects for C++ Chapter 3 Assignment 1.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <cctype>
 #include <cstring>
+#include <iomanip>
+#include <ios>
 using namespace std;
 
 int main()
@@ -17,31 +19,25 @@ int main()
     char firstLetta;
     char secondLetta;
     int length;
-
+    double moneyOwed = .16;
     cout << "What time was your call started? (use military time)" << endl;
     cin >> hour >> colon >> minute;
     cout << "What day of the week was your call (Use the first letter and the second letter to describe) " << endl;
     cin >> firstLetta >> secondLetta;
     firstLetta = tolower(firstLetta);
     secondLetta = tolower(secondLetta);
-    string theStringOne(1, firstLetta);
-    string theStringTwo(1, secondLetta);
-    string theActualString = theStringOne + theStringTwo;
     cout << "How long was your call in minutes?" << endl;
     cin >> length;
+    cout << length;
+    if ((firstLetta == 'm' && secondLetta == 'o' || firstLetta == 't' && secondLetta == 'u' || firstLetta == 'w' && secondLetta == 'e' || firstLetta == 't' && secondLetta == 'h' || firstLetta == 'f' && secondLetta == 'r') && (hour >= 8 && hour <= 17 || hour == 18 && minute == 00))
+        moneyOwed = length * 0.4;
 
-    if () {
-    }
+    if ((firstLetta == 'm' && secondLetta == 'o' || firstLetta == 't' && secondLetta == 'u' || firstLetta == 'w' && secondLetta == 'e' || firstLetta == 't' && secondLetta == 'h' || firstLetta == 'f' && secondLetta == 'r') && (hour < 8 || hour >= 18 && minute != 00))
+        moneyOwed = length * 0.25;
+
+    if (firstLetta == 's' && secondLetta == 'a' || secondLetta == 'u')
+        moneyOwed = length * 0.15;
+
+    cout << "You owe $" << fixed << setprecision(2) << moneyOwed;
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
