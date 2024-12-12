026ee7423fde1420a2f4f7d5330812bb619f9195
Ayden <133791707+The1Cook@users.noreply.github.com>
2024-12-12 14:37:22 -0600

    Update Programming Projects for C++ Chapter 4 Assignment 2.cpp

--- Programming Projects for C++ Chapter 4 Assignment 2.cpp
+++ Programming Projects for C++ Chapter 4 Assignment 2.cpp
@@ -1,13 +1,9 @@
 
 
 #include <iostream>
-
-
-
-
-
-
-
+double hat_size(double w, double h);
+double jacket_size(double w, double h, int a);
+double waist_size(double w, int a);
 
 int main()
 {
@@ -35,33 +31,32 @@ double hat_size(double w, double h)
 {
     return((w / h) * 2.9);
 }
-double jacket_size(double w, double h, double a) 
+double jacket_size(double w, double h, int a) 
 {
-    int thing = a;
-    int addition = (1 / 8) * ((thing - 30) / 10);
-    if (a - 30 <= 0) {
-        thing = 0;
-   }
-    addition = (1 / 8) * ((thing - 30) / 10);
-    return((h * w) / 288 + addition);
+    double addition;
+    if (a >= 30) {
+        addition  = .125 * ((a - 30) / 10);
+    }
+    else {
+        addition = 0;
+    }
+    
+    return(((h * w) / 288) + addition);
 }
-double waist_size(double w, double a)
+double waist_size(double w, int a)
 {
-    int thing = a;
-    int addition = .1 * ((a - 28) / 2);
-    if (a - 28 <= 28)
-        thing = 0;
-    addition = .1 * ((a - 28) / 2);
-
+    double addition;
+    if (a >= 28) {
+        addition = .1 * ((a - 28) / 2);
+    }
+    else {
+        addition = 0;
+    }
     return( (w / 5.7) + addition);
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
